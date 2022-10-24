d14df0b257c0f5213cf9de47aca455fd3fb8f1b4
wang-yuzhi-0 <nor.53@qq.com>
2022-10-24 18:18:45 +0800

    add

---
+++ 22-8-22/22-8-22.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>   //standard input output标准输入输出  .h头文件.
+int main()
+{
+    int a, b, sum;   //声明，定义a,b,sum为整型变量
+    a = 223;
+    b = 332;     //赋值
+    sum = a + b;   //进行a+b的运算，把结果存放在变量sum中
+    printf("sum is %d\n", sum);    //%d用十进制形式打印
+    return 0;
+}
+
+
+
+
+
